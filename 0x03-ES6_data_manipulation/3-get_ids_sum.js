89d40491c970b08ebabd2b7cee1552daa7f1382d
Enoosuji <Enoosuji@gmail.com>
2024-05-10 08:10:55 -0700

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,19 +1,6 @@
-i/**
- * Retrieves the sum of ids of a list of students.
- * @param {{
- *   id: Number,
- *   firstName: String,
- *   location: String
- * }[]} students - The list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- * @returns {Number}
- */
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce(
-      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
-      0,
-    );
+    return students.reduce((acc, curr) => acc + curr.id, 0);
   }
   return 0;
 }
